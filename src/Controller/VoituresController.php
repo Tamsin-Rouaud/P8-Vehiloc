d374f7ed631de95daa870beea85fce68c67bbf3e
Tamsin ROUAUD <tamsinrouaud83@gmail.com>
2025-03-30 21:42:54 +0200

    Fin étape 5 et début étape 6 avant auto-évaluation

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Voiture;
 use App\Repository\VoitureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -29,4 +30,19 @@ class VoituresController extends AbstractController
             'voiture' => $voiture,
         ]);
     }
+
+    #[Route('/voiture/car/{id}/supprimer', name: 'app_voiture_car_delete', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function deleteCar(int $id, VoitureRepository $repository, EntityManagerInterface $manager): Response
+    {
+        $voiture = $repository->find($id);
+        $manager->remove($voiture);
+        $manager->flush();
+
+        $this->redirectToRoute("app_voiture_car_delete", ['id' => $voiture->getId()]);
+
+        return $this->render('accueil.html.twig', [
+            'voiture' => $voiture,
+        ]);
+    }
+
 }
---
+++ src/Form/VoitureType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Voiture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VoitureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomVoiture')
+            ->add('description')
+            ->add('prixMensuel')
+            ->add('prixJournalier')
+            ->add('nombrePlaces')
+            ->add('status')
+            ->add('nombreKm')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Voiture::class,
+        ]);
+    }
+}
